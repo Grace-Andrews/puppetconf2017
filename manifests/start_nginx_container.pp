d6ee11a0a376c8ce02d548e82395be69556bbe8a
root <root@master.vm>
2017-09-30 18:47:25 +0000

    Add files

--- manifests/start_nginx_container.pp
+++ manifests/start_nginx_container.pp
@@ -3,6 +3,6 @@ include 'docker'
 docker::run { 'tchung02/puppet-nginx':
  ensure	=> 'present',
  image 	=> 'tchung02/puppet-nginx',
- ports	=> ['80', '443'],
- expose	=> ['80', '443'],
+ ports	=> ['80'],
+ expose	=> ['8069'],
 }
